785de96c9c42b118666f5c5bee40795942610389
Justin DeMatteis <160768253+Justindema76@users.noreply.github.com>
2026-08-15 22:37:08 -0400

    Remove temporary ConsignorsDashboard file

--- JustConsignIn-Testing-app/app/pages/consignment/ConsignorsDashboard.jsx
+++
@@ -1,7 +0,0 @@
-// Individual consignor page.
-// The existing ConsignorScreen code will be moved here one section at a time.
-// This file is intentionally not wired into consignment_intake.jsx yet.
-
-export default function ConsignorsDashboard() {
-  return null;
-}
